578c990dc7257313bc29be714e8b95ed00b7a026
Rockncoder <rockncoder@gmail.com>
2016-07-24 07:09:54 -0700

    Converts the phone-list component to Angular 2

--- app/main.ts
+++ app/main.ts
@@ -1,12 +1,16 @@
 import {UpgradeAdapter} from '@angular/upgrade';
 import {HTTP_PROVIDERS} from '@angular/http';
 import {Phone} from './core/phone/phone.service';
+import {PhoneListComponent} from './phone-list/phone-list.component';
 
 let upgradeAdapter = new UpgradeAdapter();
 
 upgradeAdapter.addProvider(HTTP_PROVIDERS);
 upgradeAdapter.addProvider(Phone);
 angular.module('core.phone').factory('phone', upgradeAdapter.downgradeNg2Provider(Phone));
+
+angular.module('phoneList')
+  .directive('phoneList', <angular.IDirectiveFactory> upgradeAdapter.downgradeNg2Component(PhoneListComponent));
 
 upgradeAdapter.bootstrap(document.documentElement, ['phonecatApp']);
 
